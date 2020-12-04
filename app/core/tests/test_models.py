7565c7338d4662cff2bfe885915b8bfa5d9fdc9b
Maksim Pevnev <drummen11@gmail.com>
2020-12-04 09:58:48 +0300

    master: added user email instead username

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,46 @@
+from unittest.mock import patch
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+from core import models
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """ Test creating a new user with an email is successful """
+        email = 'test@englishtest.com'
+        password = 'Test123456'
+
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+
+    def test_new_user_email_normalized(self):
+        """ Test the email for a new user is normalized """
+        email = 'test@ENGLISHTEST.com'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password='test124'
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """ Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'Test1234')
+
+    def test_create_new_superuser(self):
+        """ Test creating a new super user """
+        user = get_user_model().objects.create_superuser(
+            'test@englishtest.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
